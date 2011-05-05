037bcaf42e671cb2e113179969583c548b313bab
flopezluis <hola@flopezluis.es>
2011-05-05 08:19:19 +0200

    added piston

---
+++ api/handlers.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+#
+# author: javi santana
+
+from django.utils.translation import ugettext as _
+from piston.handler import AnonymousBaseHandler, BaseHandler
+from piston.utils import rc
+from django.contrib.comments.models import Comment
+from django.shortcuts import get_object_or_404
+import simplejson
+import time
+import datetime
+from django.contrib.auth.models import User
+from api.middleware import get_request
+from points.models import Point 
+from points.forms import PhotoForm
+
+class PointHandler(BaseHandler):
+    """
+        It registers an iphone to receive notifications
+    """
+
+    allowed_methos = ('POST')
+    fields = ('id','user', 'description', 'latitude', 'longitude')
+    model = Point 
+
+    def create(self, request):
+        import pdb;pdb.set_trace()
+        point, created = Point.objects.get_or_create(user=request.user, description=request.POST['description'], \
+                                    latitude=request.POST['latitude'], longitude=request.POST['longitude'])
+        if created:
+            photoForm = PhotoForm(request.POST,  request.FILES) 
+            photo = photoForm.save(commit=False)
+            photo.point = point
+            photo.save()
+
+        return rc.ALL_OK
